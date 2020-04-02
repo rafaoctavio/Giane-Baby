d1551d8b5d5be43cda7aba9624feb1f5ac7b6f99
rafaoctavio <rafaoctavio@gmail.com>
2020-04-01 22:38:45 -0300

    criadas paginas estrutuais do site

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+<footer>
+      <p><img src="img/logo_no_bg.png" width="100px" height="auto">Giane Baby | CNPJ 20.196.856/0001-91</p>
+
+    </footer>
---
+++ inc/head.php
@@ -0,0 +1,10 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Giane Baby</title>
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
