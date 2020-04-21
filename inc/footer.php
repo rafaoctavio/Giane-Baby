c92f952516b313e90ba52a7b907eb15e274ee49e
kaique <kaiquejuta3@hotmail.com>
2020-04-21 18:56:25 -0300

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,17 @@
 <footer>
-    <p><img src="img/logo_no_bg.png" width="100px" height="auto">Giane Baby | CNPJ 20.196.856/0001-91</p>
+    
+    
+    <div class="voltarAoInicio">
+        <p><a href="#carouselExampleCaptions">Voltar Ao Início</a></p>
+    </div>
+    
+    <div class="cnpj">
+        <p><img src="img/logo_no_bg.png" width="100px" height="auto">Giane Baby | CNPJ 20.196.856/0001-91</p>
+    </div>
+
+    
+        
+
+
 
 </footer>
