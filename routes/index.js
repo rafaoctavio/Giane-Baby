31ebb9e44f11281c17e069def574f5c8115a6de8
rafaoctavio <rafaoctavio@gmail.com>
2020-12-14 21:59:24 -0300

    criando logica do nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
   res.render('index');
+
+router.post('/send',(req, res) => {
+  console.log(req.body)
+
+
+
+
+  // const output = `
+  //   <p> Recebido pedido de contato</p>
+  //   <h3> Detalhes</h3>
+  //   <ul>
+  //     <li>Nome: ${req.body.nome}</li>
+  //     <li>Email: </li>
+  //     <li></li>
+      
+    
+  //   </ul>
+  
+  
+  // `;
+});
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 const nodemailer = require ('nodemailer');
 
-const transporter =nodemailer.createTransport({});
+const SMTP_CONFIG = require ('./config/smtp');
+
+const transporter =nodemailer.createTransport({
+    host: SMTP_CONFIG.host,
+    port: SMTP_CONFIG.port,
+    secure:false,
+    auth: {
+        user: SMTP_CONFIG.user,
+        pass: SMTP_CONFIG.pass
+    },
+    tls: {
+        rejectUnauthorized:false
+    }
+});
+
+async function run () {
+    const mailSent = 
+}
+
+run();
