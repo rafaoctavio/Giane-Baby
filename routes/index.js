9f74225b3df5d5ba58989f35eb218abc90395acf
rafaoctavio <rafaoctavio@gmail.com>
2020-12-16 16:50:21 -0300

    alteração no formulario

--- routes/index.js
+++ routes/index.js
@@ -6,47 +6,47 @@ const nodemailer = require('nodemailer');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.post('/send',(req, res) => {
-  const output = `
-    <p> Recebido pedido de contato</p>
-    <h3> Detalhes</h3>
-    <ul>
-      <li>Nome:${req.body.nome}</li>
-      <li>Email:${req.body.email}</li>
-      <li>Mensagem:${req.body.mensagem}</li>  
-    </ul>
-  `;
+// router.post('/send',(req, res) => {
+//   const output = `
+//     <p> Recebido pedido de contato</p>
+//     <h3> Detalhes</h3>
+//     <ul>
+//       <li>Nome:${req.body.nome}</li>
+//       <li>Email:${req.body.email}</li>
+//       <li>Mensagem:${req.body.mensagem}</li>  
+//     </ul>
+//   `;
 
-async function main() {
+// async function main() {
     
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, 
-      auth: {
-        user: "loja.gianebaby@gmail.com", 
-        pass: "giane210623"
-      },
-    });
+//     let transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false, 
+//       auth: {
+//         user: "loja.gianebaby@gmail.com", 
+//         pass: "giane210623"
+//       },
+//     });
   
-    let info = await transporter.sendMail({
-      from: '"Contato do Site GianeBaby" <loja.gianebaby@gmail.com>"',
-      to: "loja.gianebaby@gmail.com", 
-      subject: "Contato", 
-      text: "Entrar em contato",
-      html: output
-    });
+//     let info = await transporter.sendMail({
+//       from: '"Contato do Site GianeBaby" <loja.gianebaby@gmail.com>"',
+//       to: "loja.gianebaby@gmail.com", 
+//       subject: "Contato", 
+//       text: "Entrar em contato",
+//       html: output
+//     });
   
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//     console.log("Message sent: %s", info.messageId);
+//     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    res.redirect('/')
+//     res.redirect('/')
 
-  }
+//   }
   
-  main().catch(console.error);
+//   main().catch(console.error);
 
 
-});
+// });
 
 module.exports = router;
